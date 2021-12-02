73f65a8dacdb1454608a6fe7e6a7419f41c082a6
ventrosky <ventrosky@develer.com>
2021-12-02 09:12:17 +0100

    day_02

---
+++ day_02.clj
@@ -0,0 +1,50 @@
+#!/usr/bin/env bb
+(require '[clojure.string :as str])
+
+;; ./day_02.clj < ./input/day_02.txt
+(def input (str/split-lines (slurp *in*)))
+
+;; ------------------------------------ part 1
+
+(defn forward
+  [position value]
+  (assoc position :horizontal (+ (:horizontal position) value)))
+
+(defn down
+  [position value]
+  (assoc position :depth (+ (:depth position) value)))
+
+(defn up
+  [position value]
+  (assoc position :depth (- (:depth position) value)))
+
+(defn move
+ [input]
+ (let [computed (reduce (fn [position row]
+                          (let [[cmd val] (str/split row #" ")
+                                k (resolve (symbol cmd))
+                                v (Integer/parseInt val)]
+                            (apply k [position v])))
+                        {:depth 0 :horizontal 0 :aim 0}
+                        input)]
+   (* (:depth computed) (:horizontal computed))))
+
+(println "sol 1" (move input))
+
+;; ------------------------------------ part 2
+
+(defn forward
+  [position value]
+  (assoc position
+         :horizontal (+ (:horizontal position) value) 
+         :depth (+ (* value (:aim position)) (:depth position))))
+
+(defn down
+  [position value]
+  (assoc position :aim (+ (:aim position) value)))
+
+(defn up
+  [position value]
+  (assoc position :aim (- (:aim position) value)))
+
+(println  "sol 2" (move input))
